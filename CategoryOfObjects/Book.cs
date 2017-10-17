f65fec2570b30ea7c5fb078e199c15d7b6589489
Emrul Zawad <ehzawad@outlook.com>
2017-10-18 03:30:39 +0600

    Add project files.

---
+++ CategoryOfObjects/Book.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategoryOfObjects
+{
+    public class Book
+    {
+        private string bookName;
+        private string bookAuthor;
+        private string bookId;
+        private string bookType;
+        private int bookCopy;
+
+        // static members
+        static int bookCounter;
+
+
+        public Book()
+        {
+            bookCounter++;
+        }
+
+        public Book(string bookName, string bookAuthor, string bookId, string bookType, int bookCopy)
+        {
+            this.bookName = bookName;
+            this.bookAuthor = bookAuthor;
+            this.bookId = bookId;
+            this.bookType = bookType;
+            this.bookCopy = bookCopy;
+            // https://stackoverflow.com/questions/29733515/how-count-number-of-objects-created-with-a-static-class-variable
+            // this is anecdotal to C# locking mechanism, don't know the details
+            // use bookCounter++ for simplicity
+            // Interlocked.Increment(ref bookCounter);
+            bookCounter++;
+
+        }
+
+        public string BookName
+        {
+            set => this.bookName = value;
+            get => this.bookName;
+        }
+
+        public string BookAuthor
+        {
+            set => this.bookAuthor = value;
+            get => this.bookAuthor;
+        }
+
+        public string BookId
+        {
+            set => this.bookId = value;
+            get => this.bookId;
+        }
+
+        public string BookType
+        {
+            set => this.bookType = value;
+            get => this.bookType;
+        }
+
+        public int BookCopy
+        {
+            set => this.bookCopy = value;
+            get => this.bookCopy;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine("Info of the Book:");
+            Console.WriteLine("BookName: " + this.bookName);
+            Console.WriteLine("BookAuthor: " + this.bookAuthor);
+            Console.WriteLine("BookId: " + this.bookId);
+            Console.WriteLine("BookType: " + this.bookType);
+            Console.WriteLine("Number of copies of " + this.bookName + " book: " + this.bookCopy);
+            Console.WriteLine("");
+        }
+
+
+        public void AddBookCopy(int x) => this.bookCopy = this.bookCopy + x;
+
+        static Book()
+        {
+            Console.WriteLine("Welcome Console Book Application !!! \n");
+        }
+
+        public static void ShowTotalBookInfo() => Console.WriteLine("Total Number of book objects {0} !", bookCounter);
+
+    }
+
+}
---
+++ MainInterface/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CategoryOfObjects;
+
+namespace MainInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Cool");
+
+            Book csharpBook = new Book("C#", "Microsoft", "101", "Language", 5);
+            csharpBook.ShowInfo();
+
+            Book javaBook = new Book("Java By Gosling", "Oracle", "102", "Language", 2);
+            javaBook.AddBookCopy(2);
+            javaBook.ShowInfo();
+
+            Book devOps = new Book()
+            {
+                BookName = "Learn Vim",
+                BookAuthor = "Bram",
+                BookId = "103",
+                BookType = " Software development",
+                BookCopy = 2
+            };
+
+
+            Book dummy = new Book();
+            dummy.ShowInfo();
+
+            devOps.AddBookCopy(4);
+            devOps.ShowInfo();
+
+            Book.ShowTotalBookInfo();
+
+        }
+    }
+}
